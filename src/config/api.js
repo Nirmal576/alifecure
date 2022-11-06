8ee6255e2f374747bcce312980e3872956e6ad49
Nirmal576 <84316131+Nirmal576@users.noreply.github.com>
2022-11-06 16:51:35 +0530

    'intial-commit'

---
+++ src/config/api.js
@@ -0,0 +1,21 @@
+// export const API = 'https://wc2.rnlab.io';
+
+// export const CONSUMER_KEY = 'ck_9a40d82e47c124bc1be422682dd93eb25bdb5e3a';
+// export const CONSUMER_SECRET = 'cs_01bd2047d4ea33bdb4b6d8f6f034e0e072abdf4a';
+
+// export default {
+//   API_ENDPOINT: API,
+//   CONSUMER_KEY,
+//   CONSUMER_SECRET,
+// };
+
+export const API = 'https://alifecure.com';
+
+export const CONSUMER_KEY = 'ck_cad82b5686b6bed56691b11df9ea75b8bd3a4203';
+export const CONSUMER_SECRET = 'cs_f4068dfff8db7cb60a8fce05cf856d1674f24568';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+};
